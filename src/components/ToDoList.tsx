256848ae51c01f377ec72feef4833faa202c3eca
ws0671 <ws0671@naver.com>
2023-02-20 14:46:44 +0900

    6.12 Refactoring

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDo />
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
